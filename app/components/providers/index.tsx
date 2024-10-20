dec8389c69c9848b7c21dac73228387805c73ee1
Leandro Gavidia Santamaria <leandro.d3v@gmail.com>
2024-10-19 21:27:59 -0400

    add solana wallet adapter

---
+++ app/components/providers/index.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { WalletError } from "@solana/wallet-adapter-base";
+import React, { useCallback } from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+if (!process.env.NEXT_PUBLIC_RPC) {
+  throw new Error("Missing NEXT_PUBLIC_RPC env variables. Check .env.examples file")
+}
+
+export default function Providers(props: React.PropsWithChildren) {
+  const endpoint = process.env.NEXT_PUBLIC_RPC || "";
+  const onError = useCallback((error: WalletError) => {
+    console.error(error);
+  }, []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={[]} onError={onError} autoConnect>
+        <WalletModalProvider>{props.children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Header } from "./components/header";
 import { Footer } from "./components/footer";
 import { SignUpModal } from "./components/sign-up-modal";
 import { ModalProvider } from "./modal-context";
+import Providers from "./components/providers"
 
 const montserrat = Montserrat({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -23,15 +24,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <ModalProvider>
         <body className={montserrat.className}>
-          <Header />
-          <main className="max-w-[1920px] mx-auto">
-            <SignUpModal />
-            {children}
-          </main>
-          <Footer />
+          <Providers>
+            <Header />
+            <main className="max-w-[1920px] mx-auto">
+              <SignUpModal />
+              {children}
+            </main>
+            <Footer />
+          </Providers>
         </body>
       </ModalProvider>
     </html>
